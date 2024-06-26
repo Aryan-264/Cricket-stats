0af0a47b629f470549aeb219cbc4e8f31b636d96
Aryan-264 <165910796+Aryan-264@users.noreply.github.com>
2024-06-26 19:24:46 +0530

    Update dbserver.js

--- dbserver.js
+++ dbserver.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose')
 
 //const mongodb_url="mongodb+srv://aryangangwar63332:aryan2005@api.sqkn1lj.mongodb.net/myapi"  //Local database
 
-mongoose.connect(process.env.mongodb_url)
+mongoose.connect(process.env.mongodb_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    connectTimeoutMS: 30000, // Increase connection timeout to 30 seconds (default is 30000)
+    socketTimeoutMS: 45000, // Increase socket timeout to 45 seconds (default is 45000)
+});
 
 const db = mongoose.connection;
 
